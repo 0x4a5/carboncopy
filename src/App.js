5ed4247b14b65237d8f56723f26b52b2e419c432
0x4a5 <github@crisiscoins.com>
2022-06-24 23:57:44 -0700

    created section, mail, emaillist, email rows components with css style components and modified the sidebar and app and app css components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import EmailList from "./EmailList";
+import Header from "./Header";
+import Mail from "./Mail";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-    <Header />
-    <Sidebar />
+      <Header />
+
+      <div className='app_body'>
+        <Sidebar />
+        <Routes>
+        <Route path="/mail" element={<Mail />} />
+        <Route path="/" element={<EmailList />} />
+      </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/EmailRows.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./EmailRows.css";
+import { IconButton } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+
+function EmailRows({ id, title, subject, description, time }) {
+  return (
+    <div className="emailRows">
+      <div className="emailRows_options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+
+      <h3 className="emailRows_title">{title}</h3>
+
+      <div className="emailRows_message">
+        <h4>
+          {subject} {" "}
+           <span className="emailRows_description">-{description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRows_time">{time}</p>
+    </div>
+  );
+}
+
+export default EmailRows;
