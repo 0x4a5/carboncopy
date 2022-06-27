792e36a4b431070439e3df5829b047670c6d3295
0x4a5 <github@crisiscoins.com>
2022-06-27 10:22:30 -0700

    added firebase tools dependency and created form compose and close functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import EmailList from "./EmailList";
+import { selectSendMessageIsOpen } from "./features/mailSlice";
 import Header from "./Header";
 import Mail from "./Mail";
 import SendMail from "./SendMail";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen );
+
+
   return (
     <div className="app">
       <Header />
@@ -19,7 +24,7 @@ function App() {
         <Route path="/" element={<EmailList />} />
       </Routes>
       </div>
-      <SendMail/>
+      {sendMessageIsOpen && <SendMail />}
     </div>  
   );
 }
--- src/SendMail.js
+++ src/SendMail.js
@@ -1,32 +1,71 @@
-import React from 'react'
-import './SendMail.css'
-import CancelIcon from '@material-ui/icons/Cancel';
-import Button from '@material-ui/core/Button';
-
-
+import React from "react";
+import "./SendMail.css";
+import CancelIcon from "@material-ui/icons/Cancel";
+import Button from "@material-ui/core/Button";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from './features/mailSlice'
 
 function SendMail() {
-  return (
-    <div className='sendMail'>
-        <div className='sendMail_header'>
-             <h3>New Message</h3>
-             <CancelIcon className='sendMail_close' />
-        </div>
+  // const { register, handleSubmit, errors } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-        <form>
-            <input placeholder='To' type='text'/>
-            <input placeholder='Subject' type='text'/>
-            <input placeholder='Message...' type='text' className='sendMail_message'/>
-            
-            <div className='sendMail_options'>
-              <Button  
-                variant="contained"
-                color="primary" 
-                className='sendMail_send'>Send</Button>
-            </div>
-        </form>
+  const dispatch = useDispatch();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  return (
+    <div className="sendMail">
+      <div className="sendMail_header">
+        <h3>New Message</h3>
+        <CancelIcon onClick={() => dispatch(closeSendMessage())} className="sendMail_close" />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="To"
+          type="email"
+          {...register("to", { required: true })}
+        />
+        {errors.to?.type === "required" && (
+          <p className="sendMail_error">"To field is required"</p>
+        )}
+        <input
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+        />
+        {errors.subject?.type === "required" && (
+          <p className="sendMail_error">"Subject field is required"</p>
+        )}
+        <input
+          placeholder="Message..."
+          type="text"
+          className="sendMail_message"
+          {...register("message", { required: true })}
+        />
+        {errors.message?.type === "required" && (
+          <p className="sendMail_error">"Message field is required"</p>
+        )}
+        <div className="sendMail_options">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className="sendMail_send"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SendMail
+export default SendMail;
